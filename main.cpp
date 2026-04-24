f14d0639bb40214d7373e5d0e21ec504a913c636
umbertomuscillo-blip <umbertomuscillo@gmail.com>
2026-04-24 13:01:59 +0200

    aggiornamenti
    creato header della carta e cpp della carta stessa, per il lato chiaro e scuro, e aggioranto il main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "Carta.h" // Importiamo il "menu" della nostra carta
 
 using namespace std;
 
 int main() {
-    cout << "Benvenuti a UNO Flip del team da 30 e lode!" << endl;
+    cout << "Avvio simulazione UNO Flip..." << endl;
+
+    // Creiamo una carta: Lato chiaro = 1 Rosso, Lato oscuro = Pesca Cinque Arancione
+    Carta primaCarta(ROSSO, UNO, ARANCIONE, PESCA_CINQUE);
+
+    cout << "Ho creato la mia prima carta in memoria!" << endl;
+
     return 0;
 }
