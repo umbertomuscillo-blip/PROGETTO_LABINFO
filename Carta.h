5533120a2ab5b961d31965f262692cb55f0dff83
umbertomuscillo-blip <umbertomuscillo@gmail.com>
2026-04-24 13:05:48 +0200

    Update Carta.h
    aggiornamento delle carte per adattare ad uno flip

--- Carta.h
+++ Carta.h
@@ -3,29 +3,38 @@
 
 #include <string>
 
-// Definiamo i Colori possibili (sia per il lato chiaro che oscuro)
 // NERO serve per le carte Jolly che non hanno un colore di base
-enum Colore { ROSSO, GIALLO, VERDE, BLU, ROSA, VERDE_ACQUA, ARANCIONE, VIOLA, NERO };
+enum Colore { 
+    ROSSO, GIALLO, VERDE, BLU,           // Colori Lato Chiaro
+    ROSA, VERDE_ACQUA, ARANCIONE, VIOLA, // Colori Lato Oscuro
+    NERO                                 // Colore di partenza per i Jolly
+};
 
-// Definiamo i Valori possibili delle carte (numeri e speciali per entrambi i lati)
-enum Valore { ZERO, UNO, DUE, TRE, QUATTRO, CINQUE, SEI, SETTE, OTTO, NOVE, SALTA, INVERTI, PESCA_UNO, PESCA_CINQUE, SALTA_TUTTI, JOLLY, JOLLY_PESCA };
+// Enum aggiornato con le vere regole di UNO Flip! (Niente numero ZERO)
+enum Valore { 
+    UNO, DUE, TRE, QUATTRO, CINQUE, SEI, SETTE, OTTO, NOVE, 
+    SALTA,               // Lato Chiaro
+    SALTA_TUTTI,         // Lato Oscuro
+    INVERTI,             // Entrambi i lati
+    FLIP,                // Entrambi i lati (la carta che fa girare il mazzo!)
+    PESCA_UNO,           // Lato Chiaro
+    PESCA_CINQUE,        // Lato Oscuro
+    JOLLY,               // Entrambi i lati (cambia solo colore)
+    JOLLY_PESCA_DUE,     // Lato Chiaro (cambia colore e fa pescare 2)
+    JOLLY_PESCA_COLORE   // Lato Oscuro (cambia colore e fa pescare a oltranza)
+};
 
 class Carta {
 private:
-    // Attributi: i dati che ogni singola carta possiede
-    // Lato Chiaro
     Colore coloreChiaro;
     Valore valoreChiaro;
     
-    // Lato Oscuro
     Colore coloreOscuro;
     Valore valoreOscuro;
 
 public:
-    // Costruttore: il "metodo" speciale che viene chiamato quando creiamo una nuova carta
     Carta(Colore cChiaro, Valore vChiaro, Colore cOscuro, Valore vOscuro);
 
-    // Metodi (per ora dichiariamo solo che esisteranno)
     std::string getDescrizioneChiara();
     std::string getDescrizioneOscura();
 };
